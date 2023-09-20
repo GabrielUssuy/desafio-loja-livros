a276eda6d637aab89a3fcea9cdeaa073549f71f5
Gabriel Ussuy <gabriel_ussuy95@hotmail.com>
2023-09-20 19:53:35 -0300

    Setup do projeto

---
+++ src/main/java/com/ussuy/gabriel/lojalivros/DesafioLojaLivrosApplication.java
@@ -0,0 +1,12 @@
+package com.ussuy.gabriel.lojalivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLojaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLojaLivrosApplication.class, args);
+	}
+}
